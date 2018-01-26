daec134d17740fa02b4abea360b990079c7e73a6
thato1iverguy <oliverfowler5@gmail.com>
2018-01-26 15:56:38 -0500

    changes

--- src/behaviours/src/logic/search/SearchInit.h
+++ src/behaviours/src/logic/search/SearchInit.h
@@ -45,24 +45,26 @@ class SearchInit : public State
                     params.goal_y = y;
                     waypoint = new SimpleWaypoint( ssm->inputs, params );
                     ssm->waypoints.push_back( (Waypoint *)waypoint );
-                }*/
-
+                }
+                */
                 /*
                 *   Square spiral search made by Oliver.
-                */
+                *//*
                 for(int i = 0; i < 15; i ++){
                     params.goal_x = ( ( ((int)((i+4)/4))) * (pow(-1,((i+2)/2)) ) ) ;
                     params.goal_y = ( ( ((int)((i+3)/4))) * (pow(-1,((i+1)/2)) ) ) ;
-                    ssm_owner->waypoints.push_back( new SimpleWaypoint( params ) );
+                    waypoint = new SimpleWaypoint( ssm->inputs, params );
+                    ssm->waypoints.push_back( (Waypoint *)waypoint );
                 }
-                /*
+                */
+                roverID = 0; numberOfRovers = 3;
                 for(int i = 0; i < 15; i ++){
                     params.goal_x = cos( ((((2*PI)/numberOfRovers)*roverID ) * ((i%4)/2)  ) + ((((2*PI)/numberOfRovers)*(roverID+1) ) * (int)( (i+2)%4/2)) ) * (int) ((i+1)/2);
                     params.goal_y = sin( ((((2*PI)/numberOfRovers)*roverID ) * ((i%4)/2)  ) + ((((2*PI)/numberOfRovers)*(roverID+1) ) * (int)( (i+2)%4/2)) ) * (int) ((i+1)/2);
-                    
+                    std::cout << "RoverID: " << roverID << " | roverCount: " << numberOfRovers << endl;
                     waypoint = new SimpleWaypoint( ssm->inputs, params );
                     ssm->waypoints.push_back( (Waypoint *)waypoint );
-                }*/
+                }
 
                 setup_complete = true;
             }
